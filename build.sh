9c6133d9b776b0a70e0c8b66c208f5b914c63f7e
Alexander Bauer <alexander.bauer@reddoxx.com>
2017-05-10 16:34:59 +0200

    Traget registry can be change by commandline parameter

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
+
 VERSION=0.9.22
 
+TARGET_REGISTRY=$1
+[ -z "${TARGET_REGISTRY}" ] && TARGET_REGISTRY=localhost:5000
+
+if [ ${EUID} -ne 0 ]; then
+  echo "Must be run as root!"
+  exit 1
+fi
+
+# Build docker image
 export DISABLE_SYSLOG=1
 export DISABLE_SSH=1
 export DISABLE_CRON=0
 
-sudo make build NAME=baseimage
+make build NAME=baseimage
 
-docker tag baseimage:${VERSION} localhost:5000/baseimage:${VERSION}
-docker push localhost:5000/baseimage:${VERSION}
+# Push docker image
+docker tag baseimage:${VERSION} ${TARGET_REGISTRY}/baseimage:${VERSION}
+docker push ${TARGET_REGISTRY}/baseimage:${VERSION}
