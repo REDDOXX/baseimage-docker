c9c8086993830488e2118b699f71e8bb838e0ce4
Alexander Bauer <alex@familie-bauer.info>
2017-06-23 07:59:43 +0200

    Only build docker image

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-VERSION=0.9.22
-
-TARGET_REGISTRY=$1
-[ -z "${TARGET_REGISTRY}" ] && TARGET_REGISTRY=localhost:5000
-
 if [ ${EUID} -ne 0 ]; then
   echo "Must be run as root!"
   exit 1
@@ -17,6 +12,3 @@ export DISABLE_CRON=0
 
 make build NAME=baseimage
 
-# Push docker image
-docker tag baseimage:${VERSION} ${TARGET_REGISTRY}/baseimage:${VERSION}
-docker push ${TARGET_REGISTRY}/baseimage:${VERSION}
